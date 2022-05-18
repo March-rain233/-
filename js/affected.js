3141de2a4637a9873d8c420189a055b010d09eed
浅仓雨 <72779032+March-rain233@users.noreply.github.com>
2022-05-18 19:38:20 +0800

    tendency

--- js/affected.js
+++ js/affected.js
@@ -1,5 +1,7 @@
 var dataset;
 const padding = { left: 100, right: 20, top: 20, bottom: 20 };
+const width = 1920;
+const height = 1080;
 const options = ['Confirmed', 'Deaths', 'Active'];
 const maxR = 70;
 const minR = 0;
@@ -9,30 +11,28 @@ var minDate;
 var showValue = options[0];
 function dateDiff(sDate1, sDate2) {
     var aDate = sDate1.split("-");
-    var oDate1 = new Date(aDate[0], aDate[1]-1, aDate[2]);
+    var oDate1 = new Date(aDate[0], aDate[1] - 1, aDate[2]);
     aDate = sDate2.split("-");
-    var oDate2 = new Date(aDate[0], aDate[1]-1, aDate[2]);
+    var oDate2 = new Date(aDate[0], aDate[1] - 1, aDate[2]);
     var iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24);
     return iDays;
 }
-function getDate(days){
-    var dateTemp = minDate.split("-");  
+function getDate(days) {
+    var dateTemp = minDate.split("-");
     var nDate = new Date(dateTemp[1] + '-' + dateTemp[2] + '-' + dateTemp[0]); //转换为MM-DD-YYYY格式    
-    var millSeconds = Math.abs(nDate) + (days * 24 * 60 * 60 * 1000);  
-    var rDate = new Date(millSeconds);  
-    var year = rDate.getFullYear();  
-    var month = rDate.getMonth() + 1;  
-    if (month < 10) month = "0" + month;  
-    var date = rDate.getDate();  
-    if (date < 10) date = "0" + date;  
+    var millSeconds = Math.abs(nDate) + (days * 24 * 60 * 60 * 1000);
+    var rDate = new Date(millSeconds);
+    var year = rDate.getFullYear();
+    var month = rDate.getMonth() + 1;
+    if (month < 10) month = "0" + month;
+    var date = rDate.getDate();
+    if (date < 10) date = "0" + date;
     return (year + "-" + month + "-" + date);
 }
 function getSetByYear(year) {
     return dataset.find(elem => elem['Date'] == year).item;
 }
-function update(){
-    const width = 1920;
-    const height = 1080;
+function update() {
     let svg = d3.select('#main-canvas');
     let subSet = getSetByYear(getDate(document.getElementById('year-range').value));
     let xScale = d3.scale.linear()
@@ -40,7 +40,7 @@ function update(){
         .range([0, width - padding.left - padding.right]);
     let yScale = d3.scale.linear()
         .domain([d3.min(subSet.map(elem => elem[yValue])), d3.max(subSet.map(elem => elem[yValue]))])
-        .range([0, height - padding.top - padding.bottom]);
+        .range([height - padding.top - padding.bottom, 0]);
     let rScale = d3.scale.linear()
         .domain([d3.min(subSet.map((elem) => elem[showValue])), d3.max(subSet.map((elem) => elem[showValue]))])
         .range([minR, maxR]);
@@ -82,15 +82,15 @@ function create() {
             data.map((i) => {
                 if (i['Date'] == d) {
                     subSet.push({
-                        State:i['Province/State'],
-                        Country:i['Country'],
-                        Lat:parseFloat(i['Lat']),
-                        Long:parseFloat(i['Long']),
-                        Confirmed:parseInt(i['Confirmed']),
-                        Deaths:parseInt(i['Deaths']),
-                        Recovered:parseInt(i['Recovered']),
-                        Active:parseInt(i['Active']),
-                        WHO:i['WHO Region']
+                        State: i['Province/State'],
+                        Country: i['Country'],
+                        Lat: parseFloat(i['Lat']),
+                        Long: parseFloat(i['Long']),
+                        Confirmed: parseInt(i['Confirmed']),
+                        Deaths: parseInt(i['Deaths']),
+                        Recovered: parseInt(i['Recovered']),
+                        Active: parseInt(i['Active']),
+                        WHO: i['WHO Region']
                     });
                 }
             });
@@ -106,8 +106,6 @@ function create() {
             document.getElementById("year-label").innerText = getDate(range.value);
             update();
         }
-        const width = 1920;
-        const height = 1080;
         let svg = d3.select('#main-canvas')
             .attr("preserveAspectRatio", "none")
             .attr("viewBox", "0 0 " + width + " " + height);
@@ -117,7 +115,7 @@ function create() {
             .range([0, width - padding.left - padding.right]);
         let yScale = d3.scale.linear()
             .domain([d3.min(subSet.map(elem => elem[yValue])), d3.max(subSet.map(elem => elem[yValue]))])
-            .range([0, height - padding.top - padding.bottom]);
+            .range([height - padding.top - padding.bottom, 0]);
         let rScale = d3.scale.linear()
             .domain([d3.min(subSet.map((elem) => elem[showValue])), d3.max(subSet.map((elem) => elem[showValue]))])
             .range([minR, maxR]);
@@ -150,42 +148,39 @@ function create() {
             })
             .attr("r", function (d) {
                 return rScale(d[showValue]);
-            })
-            .on("mouseover", function (d) {
-                // d3.select(this)
-                //     .transition()
-                //     .attr("fill", "#71361C");
-                // var xPosition = parseFloat(d3.select(this).attr("cx"));
-                // var yPosition = parseFloat(d3.select(this).attr("cy")) + 24;
-
-                // svg.append("text")
-                //     .attr("id", "tooltip")
-                //     .attr("x", xPosition)
-                //     .attr("y", yPosition)
-                //     .attr("text-anchor", "middle")
-                //     .attr("font-family", "sans-setif")
-                //     .attr("font-size", "11px")
-                //     .attr("font-weight", "bold")
-                //     .attr("fill", "black")
-                //     .text(d['country']);
-            })
-            .on("mouseout", function (d) {
-                // d3.select(this)
-                //     .transition()
-                //     .attr("fill", colorSCale(d['gdp']));
-                // d3.select("#tooltip")
-                //     .remove();
             });
         circles.exit()
             .remove();
+        d3.selectAll('#yAxis .tick')
+            .each(function (d, i) {
+                d3.select(this).append('line')
+                    .attr('class', 'grid')
+                    .attr('stroke', 'gray')
+                    .attr('stroke-dasharray', '10')
+                    .attr('x1', 0)
+                    .attr('y1', 0)
+                    .attr('x2', width)
+                    .attr('y2', 0);
+            });
+        d3.selectAll('#xAxis .tick')
+            .each(function (d, i) {
+                d3.select(this).append('line')
+                    .attr('class', 'grid')
+                    .attr('stroke', 'gray')
+                    .attr('stroke-dasharray', '10')
+                    .attr('x1', 0)
+                    .attr('y1', 0)
+                    .attr('x2', 0)
+                    .attr('y2', -height);
+            });
     })
 }
 create();
 var selects = document.getElementById("selects").children;
-for(let i = 0; i < selects.length; ++i){
-    selects[i].onclick = ()=>{
+for (let i = 0; i < selects.length; ++i) {
+    selects[i].onclick = () => {
         showValue = options[i];
-        for(let j = 0; j < selects.length; ++j){
+        for (let j = 0; j < selects.length; ++j) {
             selects[j].className = "";
         }
         selects[i].className = "active";
--- js/tendency.js
+++ js/tendency.js
@@ -1,169 +1,129 @@
-d3.csv("covid_19_clean_complete.csv", function (error, data) {
+const padding = { left: 100, right: 20, top: 20, bottom: 20 };
+var width;
+const height = 1080;
+const interval = 100;
+var minDate;
+function dateDiff(sDate1, sDate2) {
+    var aDate = sDate1.split("-");
+    var oDate1 = new Date(aDate[0], aDate[1] - 1, aDate[2]);
+    aDate = sDate2.split("-");
+    var oDate2 = new Date(aDate[0], aDate[1] - 1, aDate[2]);
+    var iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24);
+    return iDays;
+}
+function getDate(days) {
+    var dateTemp = minDate.split("-");
+    var nDate = new Date(dateTemp[1] + '-' + dateTemp[2] + '-' + dateTemp[0]); //转换为MM-DD-YYYY格式    
+    var millSeconds = Math.abs(nDate) + (days * 24 * 60 * 60 * 1000);
+    var rDate = new Date(millSeconds);
+    var year = rDate.getFullYear();
+    var month = rDate.getMonth() + 1;
+    if (month < 10) month = "0" + month;
+    var date = rDate.getDate();
+    if (date < 10) date = "0" + date;
+    return (year + "-" + month + "-" + date);
+}
+d3.csv("../data/covid_19_clean_complete.csv", function (error, data) {
     if (error) {
         console.log(error);
     } else {
-        let dates = Array.from(new Set(data.map(elem => elem['Date'])));
-        let totalConfirmed = dates.map(elem => {
-            let t = 0;
-            for (let i = 0; i < data.length; ++i) {
-                if (data[i]['Date'] == elem) {
-                    t += Number.parseInt(data[i]['Confirmed']);
+        let times = Array.from(new Set(data.map(elem => elem['Date'])));
+        let subData = times.map(d => {
+            let active = 0;
+            let death = 0;
+            let recovered = 0;
+            let confirmed = 0;
+            let countrySort = [];
+            data.map(item => {
+                if (item['Date'] == d) {
+                    active += parseInt(item['Active']);
+                    death += parseInt(item['Deaths']);
+                    recovered += parseInt(item['Recovered']);
+                    confirmed += parseInt(item['Confirmed']);
+                    let country = countrySort.find(elem => elem['country'] == item['Country/Region']);
+                    if (country == undefined) {
+                        countrySort.push({
+                            country: item['Country/Region'],
+                            data: parseInt(item['Confirmed'])
+                        });
+                    }
+                    else {
+                        country.data += parseInt(item['Confirmed']);
+                    }
                 }
-            }
-            return t;
-        });
-        let rate = dates.map((elem, i) => {
-            if (i == 0)
-                return 0;
-            return 100 * (totalConfirmed[i] - totalConfirmed[i - 1]) / totalConfirmed[i - 1];
-        });
-        let subSet = dates.map((elem, i) => {
+            });
+            countrySort = countrySort.sort((a, b) => b.data - a.data);
             return {
-                date: elem,
-                confirmed: totalConfirmed[i],
-                rate: rate[i]
+                date: d,
+                active: active,
+                death: death,
+                recovered: recovered,
+                confirmed: confirmed,
+                countrySort: countrySort
             }
         });
-        console.log(subSet);
-        //画布周边的空白
-        var padding = { left: 100, right: 50, top: 20, bottom: 100 };
-        let rectPadding = 5;
-        var rectWidth = 150;
-        //画布大小
-        var width = (rectPadding + rectWidth) * subSet.length;
-        var height = 1080;
-        //获取SVG画布
-        var svg = d3.select("svg")
-            .attr("width", width)
-            .attr("height", height);
-        //x轴的比例尺
-        var xScale = d3.scale.ordinal()
-            .domain(dates)
+        minDate = d3.min(times);
+        let subSet = [
+            { name: 'death', data: [] },
+            { name: 'confirmed', data: [] },
+            { name: 'recovered', data: [] }
+        ];
+        subData.map(elem => {
+            subSet[0]['data'].push({ date: elem['date'], data: elem['death'] });
+            subSet[1]['data'].push({ date: elem['date'], data: elem['confirmed'] });
+            subSet[2]['data'].push({ date: elem['date'], data: elem['recovered'] });
+        });
+        subSet = d3.layout.stack()
+            .values(d => d.data)
+            .x(d => d.date)
+            .y(d => d.data)
+            (subSet);
+        width = interval * (times.length - 1) + padding.left + padding.right;
+        let svg = d3.select('#main-canvas')
+            .attr("preserveAspectRatio", "slice")
+            .attr("viewBox", "0 0 " + width + " " + height);
+        let xScale = d3.scale.ordinal()
+            .domain(times)
             .rangeBands([0, width - padding.left - padding.right]);
-        //y轴的比例尺
-        var yScale = d3.scale.linear()
-            .domain([0, d3.max(totalConfirmed)])
+        let yScale = d3.scale.linear()
+            .domain([0, d3.max(subData.map(elem => elem['confirmed']))])
             .range([height - padding.top - padding.bottom, 0]);
-        //增长率的比例尺
-        var rateScale = d3.scale.linear()
-            .domain([0, d3.max(rate)])
-            .range([height - padding.top - padding.bottom, 0]);
-        //定义x轴
-        var xAxis = d3.svg.axis()
+        var colors = d3.scale.category10();
+        let xAxis = d3.svg.axis()
             .scale(xScale)
             .orient("bottom");
-        //定义y轴
-        var yAxis = d3.svg.axis()
+        let yAxis = d3.svg.axis()
             .scale(yScale)
             .orient("left");
-        //定义rate轴
-        var rateAxis = d3.svg.axis()
-            .scale(rateScale)
-            .orient("right");
-        //添加x轴
         svg.append("g")
             .attr("class", "axis")
+            .attr('id', 'xAxis')
             .attr("transform", "translate(" + padding.left + "," + (height - padding.bottom) + ")")
             .call(xAxis);
-        //添加y轴
         svg.append("g")
             .attr("class", "axis")
+            .attr('id', 'yAxis')
             .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
             .call(yAxis);
-        //添加rate轴
-        svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(" + (width - padding.right) + "," + padding.top + ")")
-            .call(rateAxis);
-        //柱状图绘制
-        var rects = svg.selectAll(".MyRect")
-            .data(subSet);
-        rects.enter()
-            .append("rect")
-            .attr("class", "MyRect")
-            .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
-            .attr("x", function (d, i) {
-                return xScale(d['date']) + rectPadding / 2;
-            })
-            .attr("y", function (d) {
-                return yScale(d['confirmed']);
-            })
-            .attr("width", xScale.rangeBand() - rectPadding)
-            .attr("height", function (d) {
-                return height - padding.top - padding.bottom - yScale(d['confirmed']);
-            })
-            .attr("fill", "#44444f")
-            .on("mouseover", function (d) {
-                let top10 = [];
-                data.map(elem => {
-                    if (elem['Date'] == d['date']) {
-                        top10.push(elem);
-                    }
-                });
-                top1 = top10.sort((a, b)=>{
-                    return b['Confirmed'] - a['Confirmed'];
-                })
-                top10 = top10.slice(0, 10);
-                top10 = top10.map(d=>d['Country/Region']);
-                d3.select(this)
-                    .attr("fill", "#44449f");
-                console.log(top10);
-                document.getElementById("tooltip").innerText = "全世界感染人数前十国家:" + top10.toString();
-            })
-            .on("mouseout", function (d) {
-                d3.select(this)
-                    .attr("fill", "#44444f");
-                document.getElementById("tooltip").innerText = "";
-
-            });
-        rects.exit()
-            .remove();
-        // 添加折线
-        var line = d3.svg.line()
-            .x(function (d) {
-                return xScale(d['date']) + rectPadding / 2 + padding.left + rectWidth / 2;
-            })
-            .y(function (d) {
-                return rateScale(d['rate']);
-            })
-            // 选择线条的类型
-            .interpolate('linear');
-        // 添加path元素，并通过line()计算出值来赋值
-        svg.append('path')
-            .attr('stroke-width', 2)
-            .attr('d', line(subSet))
-            .attr('stroke', 'green');
-        // 添加折线
-        var line2 = d3.svg.line()
-            .x(function (d) {
-                return xScale(d['date']) + rectPadding / 2 + padding.left + rectWidth / 2;
-            })
-            .y(function (d) {
-                return padding.top + yScale(d['confirmed']);
-            })
-            // 选择线条的类型
-            .interpolate('linear');
-        // 添加path元素，并通过line()计算出值来赋值
-        svg.append('path')
-            .attr('stroke-width', 2)
-            .attr('d', line2(subSet))
-            .attr('stroke', 'red');
-        //标记绘制
-        let markBottom = 5;
-        let marks = svg.selectAll(".Mark")
+        var stack = svg.selectAll('.stakc')
             .data(subSet)
             .enter()
-            .append("text")
-            .attr("class", "Mark")
-            .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
-            .attr("x", function (d, i) {
-                return xScale(d['date']) + rectPadding / 2 + rectWidth / 4;
+            .append('g')
+            .classed('stack', true)
+            .attr('fill', function (d, i) {
+                return colors(i);
             })
-            .attr("y", function (d) {
-                return yScale(d['confirmed']) - markBottom;
-            })
-            .text(function (d) {
-                return d['confirmed'];
-            });
+        let lines = svg.selectAll('.line')
+            .data(chart.stack(data));
+        lines.enter()
+            .append('path')
+            .attr('class', (d) => 'line line-' + d.key)
+            .merge(lines)
+            .attr('fill', 'none')
+            .attr('stroke', (d, i) => chart._colors(i))
+            .transition().duration(config.animateDuration)
+            .attrTween('d', lineTween);
+        lines.exit()
+            .remove();
     }
 });
